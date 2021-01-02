86cf831633501e3df34cf37058411c161d006b7e
3-Hz <edward.h.zhao@gmail.com>
2021-01-02 15:52:10 -0600

    handle cors errors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const linus = require('./linus.json')
 const express = require('express')
+const cors = require('cors')
 const PORT = process.env.PORT || 5000
 
 let rI = (n) => Math.floor(Math.random() * n);
 
 express()
+  .use(cors())
   .get('/linus', (req, res) => {
     return res.status(200).json(linus[rI(linus.length - 1)])
   })
