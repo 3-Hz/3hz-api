96142ec1c214c50867c8eeec56446aa795fd380e
3-Hz <edward.h.zhao@gmail.com>
2021-01-01 22:51:43 -0600

    first

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const linus = require('./linus.json');
+const linus = require('./linus.json')
+const express = require('express')
+const PORT = process.env.PORT || 5000
 
 let rI = (n) => Math.floor(Math.random() * n);
 
-export default (req, res) => {
-  if (req.method === 'GET') {
-    res.status(200).json(linus[rI(linus.length)])
-  } else {
-    res.status(404)
-  }
-}
+express()
+  .get('/linus', (req, res) => {
+    return res.status(200).json(linus[rI(linus.length - 1)])
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
